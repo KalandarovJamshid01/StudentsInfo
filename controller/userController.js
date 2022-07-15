82994c292ce42c8668e59870062abcbf998b4fae
Jamshid <kalandarovjamshid01@gmail.com>
2022-07-16 02:17:28 +0500

    userModel and userController has been created

---
+++ controller/userController.js
@@ -0,0 +1,25 @@
+const User = require("./../model/userModel");
+
+const {
+  getAll,
+  getOne,
+  add,
+  update,
+  deleteData,
+} = require("./handlerController");
+
+const getAllUser = (req, res, next) => getAll(req, res, User);
+const getOneUser = (req, res, next) => getOne(req, res, User);
+const addUser = (req, res, next) => add(req, res, User);
+const updateUser = (req, res, next) => update(req, res, User);
+const deleteUser = (req, res, next) => deleteData(req, res, User);
+
+
+
+module.exports = {
+  getAllUser,
+  getOneUser,
+  addUser,
+  updateUser,
+  deleteUser,
+};
--- model/userModel.js
+++ model/userModel.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -7,5 +10,38 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    validate: [
+      validator.isEmail,
+      "This email is ignore.Please write correct email",
+    ],
+    unique: true,
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: true,
+  },
+  passwordConfirm: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (val) {
+        return val === this.password;
+      },
+      message: "Siz bir xil password kiritishingiz kerak",
+    },
+  },
+  role: {
+    type: String,
+    enum: ["admin", "student", "teacher"],
+    default: "student",
+  },
+  active: {
+    type: Boolean,
+    default: true,
   },
 });
+
+const User = mongoose.model("users", userSchema);
+
+module.exports = User;
